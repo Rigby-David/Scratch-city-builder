269ce9148bac7b1c1d8922837e9dfda43fe8369f
David <Rigby335@hotmail.com>
2022-04-28 12:06:46 -0700

    checked state

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ const houseDropdown = document.getElementById('house-dropdown');
 
 
 // let state
+let stateCount = 0;
+let cityCount = 0;
+let houseCount = 0;
 
-// set event listeners 
+// set event listeners
+stateDropdown.addEventListener('change', () => {
+// console.log('showing', stateDropdown.value);
+stateCount++;
+
+});
+
+cityDropdown.addEventListener('change', () => {
+// console.log('showing', cityDropdown.value);
+cityCount++;
+
+});
+
+houseDropdown.addEventListener('change', () => {
+// console.log('showing', houseDropdown.value);
+houseCount++;
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
