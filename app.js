e6bec60fd30bcbe4a19b1536127b16e9810e0428
David <Rigby335@hotmail.com>
2022-04-29 15:16:42 -0700

    added HTMl structure, got elements by id, added event listeners

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 // import functions and grab DOM elements
-const treeDropdown = document.getElementById('tree-dropdown');
-const groundDropdown = document.getElementById('city-dropdown');
-const animalDropdown = document.getElementById('house-dropdown');
-
-const treeDiv = document.getElementById('tree');
-
+// We use document.getElementById('') to grab our HTML elements and give them a variable to use in app.js
+const treeEl = document.getElementById('trees');
+const groundEl = document.getElementById('ground');
+const animalEl = document.getElementById('animal');
 
 // let state
-let treeCount = 0;
-let groundCount = 0;
-let animalCount = 0;
+// state is everything on the page that will change based off user input
+// we use let to name a variable
+// if a number data type, let variable = 0;
+// if an array data type, let variable = [];
+let count = 0;
 
 // set event listeners
-TreeDropdown.addEventListener('change', () => {
-// console.log('showing', stateDropdown.value);
-    treeCount++;
-    treeDiv.style.backgroundImage = `url('./assets/${treeDropdown.value}.jpg')`;
-});
-
-groundDropdown.addEventListener('change', () => {
-// console.log('showing', cityDropdown.value);
-groundCount++;
+// we use variableName.addEventListener('click/change', () => {})
+// this allows our buttons/selects to listen to our code and allows user to change state
+// in here we need to call our state variables, and any display functions that take action when the event listener is called
+// if we want an image to pull display on event listener
+// we use variable.style.backgroundImage = `url('./assets/${}.jpg/png')`; to call our image from assets
+treeEl.addEventListener('change', () => {
 
 });
 
-animalDropdown.addEventListener('change', () => {
-// console.log('showing', houseDropdown.value);
-animalCount++;
+groundEl.addEventListener('change', () => {
 
 });
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+animalEl.addEventListener('change', () => {
+
+});
+// use user input to update state
+// we use template literals `${}`
