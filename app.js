521d564b589b3b7c35f1762318cbd7c6cddb9bf3
David <Rigby335@hotmail.com>
2022-04-29 15:35:45 -0700

    checked variable names and event listeners

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // import functions and grab DOM elements
 // We use document.getElementById('') to grab our HTML elements and give them a variable to use in app.js
-const treeEl = document.getElementById('trees');
-const groundEl = document.getElementById('ground');
-const animalEl = document.getElementById('animal');
+const treeSection = document.getElementById('tree-section');
+const groundSection = document.getElementById('ground-section');
+const animalSection = document.getElementById('animal-section');
+
+const treeSelect = document.getElementById('tree-select');
+const groundSelect = document.getElementById('ground-select');
+const animalSelect = document.getElementById('animal-select');
+
+const treeDiv = document.getElementById('tree');
+const groundDiv = document.getElementById('ground');
+const animalDiv = document.getElementById('ground');
 
 // let state
 // state is everything on the page that will change based off user input
@@ -17,16 +25,19 @@ let count = 0;
 // in here we need to call our state variables, and any display functions that take action when the event listener is called
 // if we want an image to pull display on event listener
 // we use variable.style.backgroundImage = `url('./assets/${}.jpg/png')`; to call our image from assets
-treeEl.addEventListener('change', () => {
+treeSelect.addEventListener('change', () => {
+console.log('hi', treeSelect);
+});
+
+groundSelect.addEventListener('change', () => {
+console.log('hi', groundSelect);
 
 });
 
-groundEl.addEventListener('change', () => {
-
-});
-
-animalEl.addEventListener('change', () => {
+animalSelect.addEventListener('change', () => {
+console.log('hi', animalSelect);
 
 });
 // use user input to update state
 // we use template literals `${}`
+``
